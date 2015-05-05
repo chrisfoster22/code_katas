8f990c0ea63f4c434a6958010a901b894ba2c5ef
Chris Foster <fosterchris22@gmail.com>
2015-05-05 14:38:35 -0400

    Two tests passing.

--- coin_changer/coin_changer.rb
+++ coin_changer/coin_changer.rb
@@ -1,6 +1,13 @@
 class CoinChanger
 
   def initialize(amount)
+    @amount = amount
+  end
+
+  def change
+    coins = []
+    @amount.times { coins << 1 }
+    coins
   end
 
 end
--- coin_changer/spec/coin_changer_spec.rb
+++ coin_changer/spec/coin_changer_spec.rb
@@ -3,7 +3,14 @@ require_relative '../coin_changer.rb'
 
 RSpec.describe CoinChanger do
 
-  it "accepts an integer amount" do
-    changer = CoinChanger.new(100)
+  it "returns a penny" do
+    changer = CoinChanger.new(1)
+    expect(changer.change).to eql [1]
   end
+
+  it "returns two pennies" do
+    changer = CoinChanger.new(2)
+    expect(changer.change).to eql [1, 1]
+  end
+
 end
